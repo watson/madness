8b02442b0f89ba4ea7ad99d9c5672f932deb11fe
Thomas Watson Steen <w@tson.dk>
2014-08-08 20:18:44 +0200

    Show a nice cow

--- blow.js
+++ blow.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var util = require('util');
 var http = require('http');
 var pump = require('pump');
+var cow = require('cowsay');
 var requestStats = require('request-stats');
 var prettyBytes = require('pretty-bytes');
 
@@ -12,8 +14,9 @@ var server = http.createServer(function (req, res) {
 
 requestStats(server, function (stats) {
   var size = prettyBytes(stats.res.bytes);
-  var avg = prettyBytes(stats.res.bytes / (stats.time / 1000));
-  console.log('Downloaded %s at %s/s', size, avg);
+  var speed = prettyBytes(stats.res.bytes / (stats.time / 1000));
+  var msg = util.format('Hi man! Fancy a beer? %s @ %s/s', size, speed);
+  process.stdout.write('\x1b[2J\x1b[H' + cow.say({ text: msg, f: myCow }));
 });
 
 function madness() {
@@ -25,3 +28,5 @@ function madness() {
     }, Math.random() * 500);
   });
 }
+
+var myCow = process.argv[2] || ['beavis.zen','eyes','milk','stimpy','bong','flaming-sheep','moofasa','supermilker','bud-frogs','ghostbusters','moose','surgery','bunny','head-in','mutilated','telebears','cheese','hellokitty','ren','turkey','cower','kiss','satanic','turtle','daemon','kitty','sheep','tux','default','koala','skeleton','vader-koala','dragon-and-cow','kosh','small','vader','dragon','luke-koala','sodomized','www','elephant-in-snake','mech-and-cow','squirrel','elephant','meow','stegosaurus'][Math.random() * 45 | 0];
